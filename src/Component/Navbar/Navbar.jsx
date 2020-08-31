4d6bb95194e11e25172e763f8d8e6a08775e8297
fauziapriana29 <fauzicoding@gmail.com>
2020-08-31 22:20:22 +0700

    re-design navbar

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ class Navbar extends Component  {
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <a className="navbar-brand" href="#">Travelink.com</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
