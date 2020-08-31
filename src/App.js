4de4871d4aacf773fda7f00f768479453fef2c6d
fauziapriana29 <fauzicoding@gmail.com>
2020-08-31 21:04:43 +0700

    try 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'; 
 import './App.css';
+import Navbar from './Component/Navbar/Navbar.jsx'
 
 function App() {
   return (
-    <div className="container">
-      
+    <div className>
+      < Navbar /> 
     </div>
   );
 }
