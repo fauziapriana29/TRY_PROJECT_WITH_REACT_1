2061bc39d82c22ba1d7a5d071400e823635b3a5c
fauziapriana29 <fauzicoding@gmail.com>
2020-09-01 10:30:19 +0700

    add input text for search field

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './Navbar.css'
+import img from './icon.png'
 
 class Navbar extends Component  {
     render() {
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light">
-                    <a className="navbar-brand" href="#">Travelink.com</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                    <img src={img} alt="logo" className="nav-logo"/>
+                    <a className="brand-navbar" href="#">Travelink.com</a>
+                    <input type="text" className="search-input form" placeholder="Search.."/>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item active">
@@ -22,7 +22,7 @@ class Navbar extends Component  {
                                 <a className="nav-link" href="#">About Us</a>
                             </li>
                             <li className="nav-item active">
-                                <a className="nav-link" href="#">Contact Service</a>
+                                <a className="nav-link" href="#">Contact</a>
                             </li>
                         </ul>
                     </div>
